67fee4a51aca764ad26ebb8f1547b81969289441
dmytrotsymbal <dima.tsymbal@outlook.com>
2024-06-01 13:43:08 +0300

    fix env

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const axios = require("axios");
 const fs = require("fs").promises;
 const path = require("path");
 
-const API_KEY = process.env.API_KEY;
+const API_KEY = process.env.REACT_APP_API_KEY;
 const PORT = 8000;
 
 const app = express();
